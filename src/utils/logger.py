d9abd7e30892fc132d68b71d5d6b58c2c2919413
Sylvester <slyamby@gmail.com>
2026-07-30 14:25:57 +0000

    Create log directory before configuring logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,6 +5,11 @@ from config.settings import LOG_DIR
 
 LOG_FILE = LOG_DIR / "pipeline.log"
 
+LOG_FILE.parent.mkdir(
+    parents=True, 
+    exist_ok=True
+    )
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)s | %(message)s",
